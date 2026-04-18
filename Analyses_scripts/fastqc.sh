988db3a56298bec65da5a462249ab74cd72f5838
ZONGOGITHUB <zongosaidou642@gmail.com@ird.fr>
2026-04-18 00:36:56 +0000

    update somes files

---
+++ Analyses_scripts/fastqc.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#SBATCH --job-name=fastqc
+#SBATCH -p normal
+#SBATCH -c 4
+#SBATCH --array=0-76%4
+#SBATCH --output=QC/logs/fastqc_%A_%a.out
+#SBATCH --error=QC/logs/fastqc_%A_%a.err
+
+module load fastqc/0.12.1
+
+Input_dir="RAW_DATA"
+Output_dir="QC/fastqc_results"
+Threads=4
+
+mkdir -p "$Output_dir" QC/logs
+
+# Liste des fichiers R1
+samples=("$Input_dir"/*_R1.fastq.gz)
+sample="${samples[$SLURM_ARRAY_TASK_ID]}"
+base=$(basename "$sample" _R1.fastq.gz)
+
+R1="$Input_dir/${base}_R1.fastq.gz"
+R2="$Input_dir/${base}_R2.fastq.gz"
+
+# Vérification de la paire
+if [[ ! -f "$R1" || ! -f "$R2" ]]; then
+    echo "Missing pair: $base"
+    exit 1
+fi
+
+echo "Processing sample: $base"
+
+# Lancer FastQC directement dans Output_dir
+fastqc -t "$Threads" -o "$Output_dir" "$R1" "$R2"
+
