84d22b62d92ee4c6de8806cc72a5950cb96c7649
ZONGOGITHUB <zongosaidou642@gmail.com@ird.fr>
2026-05-08 07:44:09 +0000

    Update mapping.sh

---
+++ Scripts/mapping.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+# Slurm configuration
+#SBATCH --job-name=mapping
+#SBATCH --partition=normal
+#SBATCH --cpus-per-task=12
+#SBATCH --output=/scratch/zongo/CIBIG_Internship_Project/logs/mapping_%j.out
+#SBATCH --error=/scratch/zongo/CIBIG_Internship_Project/logs/mapping_%j.err
+#SBATCH --nodelist=node02
+
+set -euo pipefail
+
+# Directories
+INPUT_DIR="/scratch/zongo/CIBIG_Internship_Project/Results/QC/Trimmomatic"
+OUTPUT_DIR="/scratch/zongo/CIBIG_Internship_Project/Mapping"
+REF_GENOME="/scratch/zongo/CIBIG_Internship_Project/Ref/GCF_000002495.2_MG8_genomic.fna"
+
+mkdir -p "$OUTPUT_DIR"
+
+# Modules loading
+module load bioinfo-wave
+module load bwamem2/2.3
+module load samtools/1.23.1
+
+# Ref genome indexing
+if [[ ! -f "${REF_GENOME}.bwt.2bit.64" ]]; then
+    echo "Index BWA inexistant, création en cours..."
+    bwa-mem2 index "$REF_GENOME"
+    echo "Index créé."
+else
+    echo "Index BWA trouvé, utilisation de l'existant."
+fi
+
+# Loop on the sequences
+for R1 in "$INPUT_DIR"/*_R1_paired.fastq.gz; do
+
+    # Samples name from R1
+    sample=$(basename "$R1" _R1_paired.fastq.gz)
+    # Construction du chemin du fichier R2 correspondant
+    R2="$INPUT_DIR/${sample}_R2_paired.fastq.gz"
+
+    # Output directories
+    BAM_FILE="$OUTPUT_DIR/${sample}.bam"
+    STATS_FILE="$OUTPUT_DIR/${sample}_stats.txt"
+    FILTERED_FILE="$OUTPUT_DIR/${sample}_filtered.bam"
+    SORTED_FILE="$OUTPUT_DIR/${sample}_sorted.bam"
+
+    # Mapping
+    bwa-mem2 mem -t 12 "$REF_GENOME" "$R1" "$R2" | samtools view -@ 12 -Sb - > "$BAM_FILE"
+
+    # Bam statistics
+    samtools flagstat "$BAM_FILE" > "$STATS_FILE"
+
+    # Bam filtering
+    samtools view -b -q 30 "$BAM_FILE" > "$FILTERED_FILE"
+    rm -f "$BAM_FILE"
+
+    # filtered.bam sorting with MAPQ >= 30
+    samtools sort -o "$SORTED_FILE" "$FILTERED_FILE"
+    rm -f "$FILTERED_FILE"
+
+    # Sorted.bam indexing
+    samtools index "$SORTED_FILE"
+
+    echo "✅ Terminé pour $sample"
+done
